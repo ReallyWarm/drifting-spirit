433a3181438505286bed81cfbeb57ca2d1f19171
Walan <warmkitjarak@gmail.com>
2022-10-16 21:20:45 +0700

    update dash

--- engine/player.py
+++ engine/player.py
@@ -10,7 +10,10 @@ class Player(pygame.sprite.Sprite):
         self.ani = {'run' : Animate("sprite/player-run.png", (0,0,24,32), 8, pixel_jump=2, frames=3),
                     'idle': Animate("sprite/player-idle.png",(0,0,24,32), 4, pixel_jump=2, frames=45),
                     'fall': Animate("sprite/player-fall.png",(0,0,24,32), 1),
-                    'jump': Animate("sprite/player-jump.png",(0,0,24,32), 1)}
+                    'jump': Animate("sprite/player-jump.png",(0,0,24,32), 1),
+                    'dshX': Animate("sprite/player-dashes.png",(0,0,24,32), 1),
+                    'dshD': Animate("sprite/player-dashes.png",(2,0,24,32), 1, start_at=1),
+                    'dshU': Animate("sprite/player-dashes.png",(2,0,24,32), 1, start_at=2)}
         self.image = self.ani['idle'].image
         self.rect = self.image.get_rect(midbottom = (150,300))
 
@@ -21,10 +24,14 @@ class Player(pygame.sprite.Sprite):
         self.face_right = True
         self.moveL = False
         self.moveR = False
+        self.moveU = False
         self.moveD = False
         self.jump_time = [0,15]
         self.jumped = False
         self.jumping = False
+        self.dashing = False
+        self.dash_direct = 0
+        self.dash_time = [0,16]
         
         self.pos = Vector2(self.rect.topleft)
         self.vel = Vector2(0,0)
@@ -37,24 +44,45 @@ class Player(pygame.sprite.Sprite):
                     self.moveL = True
                 if event.key == pygame.K_d:
                     self.moveR = True
+                if event.key == pygame.K_w:
+                    self.moveU = True
+                if event.key == pygame.K_s:
+                    self.moveD = True
                 if event.key == pygame.K_j:
                     if not self.jumping:
                         self.jumped = True
-                if event.key == pygame.K_s:
-                    self.moveD = True
+                if event.key == pygame.K_k:
+                    self.dashing = True
+                    if self.moveU:
+                        self.dash_direct = 1
+                    elif self.moveD:
+                        self.dash_direct = 2
+                    elif self.face_right:
+                        self.dash_direct = 3
+                    else:
+                        self.dash_direct = 4
 
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_a:
                     self.moveL = False
                 if event.key == pygame.K_d:
                     self.moveR = False
-                if event.key == pygame.K_j:
-                    self.jumped = False
+                if event.key == pygame.K_w:
+                    self.moveU = False
                 if event.key == pygame.K_s:
                     self.moveD = False
+                if event.key == pygame.K_j:
+                    self.jumped = False
 
     def update(self, dt):
-        if self.vel.y > 1:
+        print(self.dash_direct)
+        if self.dash_direct == 1:
+            self.state = 'dshU'
+        elif self.dash_direct == 2:
+            self.state = 'dshD'
+        elif self.dashing:
+            self.state = 'dshX'
+        elif self.vel.y > 1:
             self.state = 'fall'
         elif self.vel.y < 0:
             self.state = 'jump'
@@ -94,23 +122,52 @@ class Player(pygame.sprite.Sprite):
         else:
             direction_x = 0
 
-        if self.jumped and self.jump_time[0] < self.jump_time[1] / dt:
-            self.vel.y = -3.6 * dt
-            self.jump_time[0] += 1
-            self.jumping = True
-        else:
-            self.jumped = False
-            self.jump_time[0] = 0
+        # Dashing take priority
+        if self.dashing:
+            if self.dash_time[0] < self.dash_time[1] / dt:
+                self.vel = Vector2(0,0)
+                self.dash_time[0] += 1
+                dash_x = 4 * dt
+                dash_y = 5 * dt
 
-        # Running    
-        self.acc.x = direction_x * acc * dt
-        self.acc.x += self.vel.x * fric
-        self.vel += self.acc * dt
-        self.vel.y = (max_y * dt) if (self.vel.y > max_y * dt) else (self.vel.y)
+                if self.dash_direct == 1:
+                    self.vel.y = -dash_y
+                elif self.dash_direct == 2:
+                    self.vel.y = dash_y
+                elif self.dash_direct == 3:
+                    self.vel.x = dash_x
+                elif self.dash_direct == 4:
+                    self.vel.x = -dash_x
+            else:
+                self.dashing = False
+                self.dash_direct = 0
+                self.dash_time[0] = 0
+                self.vel /= 4
+
+        # Jump and Run
+        else:
+            # Jumping
+            if self.jumped and self.jump_time[0] < self.jump_time[1] / dt:
+                self.vel.y = -3.6 * dt
+                self.jump_time[0] += 1
+                self.jumping = True
+            else:
+                self.jumped = False
+                self.jump_time[0] = 0
+
+            # Running    
+            self.acc.x = direction_x * acc * dt
+            self.acc.x += self.vel.x * fric
+
+            # Update velocity
+            self.vel += self.acc * dt
+            if self.vel.y > max_y * dt:
+                self.vel.y = max_y * dt
+
         self.pos += self.vel + (0.5 * self.acc)
 
         # Stopping
-        if self.vel.x < 0.1 * dt and self.vel.x > -0.1 * dt:
+        if self.vel.x < 0.08 * dt and self.vel.x > -0.08 * dt:
             self.vel.x = 0
 
         # Update position
@@ -130,7 +187,7 @@ class Player(pygame.sprite.Sprite):
 
         new_rect.y = round(self.pos.y)
         # collide platform
-        if self.moveD == False:
+        if not self.moveD:
             for platform in collision_platform:
                 if platform.rect.colliderect(new_rect):
                     if self.vel.y > 0:
