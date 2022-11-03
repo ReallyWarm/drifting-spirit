8196201e9da5164b2e0e152325dd40ebb92e09dd
Walan <warmkitjarak@gmail.com>
2022-11-04 01:54:23 +0700

    pause and score

--- engine/menu.py
+++ engine/menu.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, base64, json
 from engine.game import Game
 from engine.graphic.gameui import Button
 
@@ -12,40 +12,66 @@ class Menu():
         self.screen = pygame.display.set_mode(self.scr_size, 0, 32)
         self.scale = (1,1)
 
-        self.current_menu = [self.main_menu, self.game_loop, self.rank_menu, self.option_menu]
+        self.current_menu = [self.main_menu, self.game_loop, self.rank_menu, self.option_menu, self.score_menu]
         self.id = 0
+        self.pause = False
+
+        with open('data/rank.json', 'rb') as f:
+            # load data str
+            try:
+                self.rank_data = json.load(f)
+                # decode to base64 then decode to str
+                self.rank_data = base64.b64decode(self.rank_data).decode('utf-8')
+                # convert to dict
+                self.rank_data = json.loads(self.rank_data)
+            except json.decoder.JSONDecodeError:
+                print(1)
+                self.rank_data = dict()
+            f.close()
 
         self.main_menu_button = pygame.sprite.Group()
         self.option_menu_button = pygame.sprite.Group()
+        self.pause_menu_button = pygame.sprite.Group()
         self.back_button = None
-        self.set_button()
+        self.set_menu()
 
         self.game = Game((256,320), self.scale)
         self.new_game = True
         self.run = True
 
-    def set_button(self):
+    def set_menu(self):
         sw, sh = self.scale
-        self.fontM = pygame.font.SysFont(None, int(30*sh))
-        self.fontOp = pygame.font.SysFont(None, int(25*sh))
+        self.fontL = pygame.font.SysFont(None, int(30*sh))
+        self.fontM = pygame.font.SysFont(None, int(25*sh))
 
-        self.back_button = Button(self.scr_size[0] - 80*sw, 30*sh, 50*sw, 50*sh, (200,155,155), self.fontM, '->')
+        self.back_button = Button(self.scr_size[0] - 80*sw, 30*sh, 50*sw, 50*sh, (200,155,155), self.fontL, '->')
 
         self.main_menu_button.empty()
-        self.main_menu_button.add(Button(80*sw, self.scr_size[1] - 370*sh, 200*sw, 50*sh, (40,50,150), self.fontM, 'Play'))
-        self.main_menu_button.add(Button(80*sw, self.scr_size[1] - 290*sh, 200*sw, 50*sh, (40,50,150), self.fontM, 'Ranking'))
-        self.main_menu_button.add(Button(80*sw, self.scr_size[1] - 210*sh, 200*sw, 50*sh, (40,50,150), self.fontM, 'Options'))
-        self.main_menu_button.add(Button(80*sw, self.scr_size[1] - 130*sh, 200*sw, 50*sh, (40,50,150), self.fontM, 'Exit'))
+        self.main_menu_button.add(Button(80*sw, self.scr_size[1] - 370*sh, 200*sw, 50*sh, (40,50,150), self.fontL, 'Play'))
+        self.main_menu_button.add(Button(80*sw, self.scr_size[1] - 290*sh, 200*sw, 50*sh, (40,50,150), self.fontL, 'Ranking'))
+        self.main_menu_button.add(Button(80*sw, self.scr_size[1] - 210*sh, 200*sw, 50*sh, (40,50,150), self.fontL, 'Options'))
+        self.main_menu_button.add(Button(80*sw, self.scr_size[1] - 130*sh, 200*sw, 50*sh, (40,50,150), self.fontL, 'Exit'))
 
         self.option_menu_button.empty()
-        self.option_menu_button.add(Button(80*sw, 150*sh, 100*sw, 50*sh, (40,50,150), self.fontOp, f'{self.scr_option[0][0]}x{self.scr_option[0][1]}'))
-        self.option_menu_button.add(Button(80*sw, 230*sh, 100*sw, 50*sh, (40,50,150), self.fontOp, f'{self.scr_option[1][0]}x{self.scr_option[1][1]}'))
-        self.option_menu_button.add(Button(80*sw, 310*sh, 100*sw, 50*sh, (40,50,150), self.fontOp, f'{self.scr_option[2][0]}x{self.scr_option[2][1]}'))
-        self.option_menu_button.add(Button(80*sw, 390*sh, 100*sw, 50*sh, (40,50,150), self.fontOp, f'{self.scr_option[3][0]}x{self.scr_option[3][1]}'))
+        self.option_menu_button.add(Button(80*sw, 200*sh, 100*sw, 50*sh, (40,50,150), self.fontM, f'{self.scr_option[0][0]}x{self.scr_option[0][1]}'))
+        self.option_menu_button.add(Button(80*sw, 280*sh, 100*sw, 50*sh, (40,50,150), self.fontM, f'{self.scr_option[1][0]}x{self.scr_option[1][1]}'))
+        self.option_menu_button.add(Button(80*sw, 360*sh, 100*sw, 50*sh, (40,50,150), self.fontM, f'{self.scr_option[2][0]}x{self.scr_option[2][1]}'))
+        self.option_menu_button.add(Button(80*sw, 440*sh, 100*sw, 50*sh, (40,50,150), self.fontM, f'{self.scr_option[3][0]}x{self.scr_option[3][1]}'))
+
+        self.rank_board = pygame.Surface((760*sw,600*sh))
+        self.rank_board.fill((200,155,155))
+
+        self.pause_menu_button.empty()
+        self.pause_menu_button.add(Button(self.scr_size[0]//2 - 75*sw, 200*sh, 150*sw, 50*sh, (40,50,150), self.fontL, 'Resume'))
+        self.pause_menu_button.add(Button(self.scr_size[0]//2 - 75*sw, 280*sh, 150*sw, 50*sh, (40,50,150), self.fontL, 'Back to Title'))
+        self.pause_menu_tint = pygame.Surface(self.scr_size, pygame.SRCALPHA)
+        self.pause_menu_tint.fill((77, 77, 92))
+        self.pause_menu_tint.set_alpha(120)
 
     def run_menu(self, event_list, dt):
         if self.id == 1 and self.new_game:
             self.game.init_level()
+            self.score = dict()
             self.new_game = False
 
         self.current_menu[self.id](event_list, dt)
@@ -68,29 +94,38 @@ class Menu():
         self.main_menu_button.draw(self.screen)
 
     def game_loop(self, event_list, dt):
-        self.back_button.update(event_list)
+        if not self.pause:
+            self.back_button.update(event_list)
 
-        if pygame.key.get_pressed()[pygame.K_ESCAPE] or self.back_button.get_clicked() or not self.game.running:
-            self.id = 0
-            self.new_game = True
+            if pygame.key.get_pressed()[pygame.K_ESCAPE] or self.back_button.get_clicked():
+                self.pause = True
+                self.game.reset_player_move()
 
-        self.game.update(event_list, dt)
+            self.game.update(event_list, dt)
+
+            if not self.game.running:
+                self.calculate_score()
+                self.id = 4
+                self.new_game = True
             
         self.screen.fill((30,30,30))
         self.game.draw()
-        self.back_button.draw(self.screen)
+
+        if self.pause:
+            self.pause_menu(event_list)
+        else:
+            self.back_button.draw(self.screen)
 
     def rank_menu(self, event_list, _):
         sw, sh = self.scale
         self.back_button.update(event_list)
-        boardB = pygame.Rect((self.scr_size[0]-760*sw)/2,(self.scr_size[1]-600*sh)/2,760*sw,600*sh)
 
         if pygame.key.get_pressed()[pygame.K_ESCAPE] or self.back_button.get_clicked():
             self.id = 0
             
         self.screen.fill((200,220,255))
         self.back_button.draw(self.screen)
-        pygame.draw.rect(self.screen, (200,155,155), boardB)
+        self.screen.blit(self.rank_board, ((self.scr_size[0]-760*sw)/2,(self.scr_size[1]-600*sh)/2))
 
     def option_menu(self, event_list, _):
         self.back_button.update(event_list)
@@ -105,9 +140,80 @@ class Menu():
                 self.scr_size = self.scr_option[self.scr_id]
                 self.scale = (self.scr_size[0] / self.scr_default[0], self.scr_size[1] / self.scr_default[1])
                 self.screen = pygame.display.set_mode(self.scr_size, 0, 32)
-                self.set_button()
+                self.set_menu()
                 self.game.update_surface(self.scale)
   
         self.screen.fill((100,220,155))
         self.back_button.draw(self.screen)
         self.option_menu_button.draw(self.screen)
+
+    def score_menu(self, event_list, _):
+        self.back_button.update(event_list)
+
+        if pygame.key.get_pressed()[pygame.K_ESCAPE] or self.back_button.get_clicked():
+            self.id = 0
+
+            anyms_c = 1
+            for name in self.rank_data:
+                if name.startswith('anonymous') and anyms_c < 5:
+                    anyms_c += 1
+
+            if self.rank_data[f'anonymous{anyms_c}'] < self.score['all']:
+                self.rank_data[f'anonymous{anyms_c}'] = self.score['all']
+
+            while len(self.rank_data) > 5:
+                rm_name = min(self.rank_data, key=self.rank_data.get)
+                print(rm_name)
+                self.rank_data.pop(rm_name)
+
+            with open('data/rank.json', 'w') as f:
+                data = self.rank_data
+                # convert to json dict then encode to byte
+                data = json.dumps(data).encode('utf-8')
+                # encode to base64 then decode to str
+                data = base64.b64encode(data).decode('utf-8')
+                # write to file
+                json.dump(data, f)
+                f.close()
+
+            print(self.rank_data)
+
+        self.screen.fill((100,220,155))
+        self.back_button.draw(self.screen)
+
+    def pause_menu(self, event_list):
+        self.pause_menu_button.update(event_list)
+
+        if pygame.key.get_pressed()[pygame.K_ESCAPE]:
+            self.pause = False
+        for i, button in enumerate(self.pause_menu_button.sprites()):
+            if button.get_clicked():
+                if i == 1:
+                    self.game.quit_game()
+                    self.calculate_score()
+                    self.id = 4
+                    self.new_game = True
+                self.pause = False
+
+        self.screen.blit(self.pause_menu_tint, (0,0))
+        self.pause_menu_button.draw(self.screen)
+
+    def calculate_score(self):
+        score_data = self.game.score_data
+
+        self.score['height'] = int(score_data['height'] * 0.7)
+        self.score['enemy'] = dict()
+        self.score['enemy']['ght'] = score_data['enemy']['ght'] * 100
+        self.score['enemy']['imp'] = score_data['enemy']['imp'] * 300
+        self.score['item'] = dict()
+        self.score['item']['ts1'] = score_data['item']['ts1'] * 200
+        self.score['item']['th1'] = score_data['item']['th1'] * 150
+
+        self.score['health'] = 0
+        if score_data['height'] >= 100:
+            self.score['health'] = score_data['health'] * 100
+
+        self.score['all'] = self.score['height']+self.score['enemy']['ght']+self.score['enemy']['imp']+\
+                            self.score['item']['ts1']+self.score['item']['th1']+self.score['health']
+
+        print(self.score)
